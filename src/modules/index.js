285f9d55ed4ac3778d5588bc4e02af4236f15f6f
mrmoodswings <safwan.mst007@gmail.com>
2021-11-04 01:44:31 +0530

    auth

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,8 +4,8 @@ const users = require("./users/users.routes");
 const products = require('./products/products.routes');
 const orders = require('./orders/orders.routes');
 module.exports = function (app) {
-  app.use("/api/auth", auth);
-  app.use("/api/users", users);
-  app.use("/api/products",products);
-  app.use("/api/orders",orders);
+  app.use("auth", auth);
+  app.use("users", users);
+  app.use("products",products);
+  app.use("orders",orders);
 };
